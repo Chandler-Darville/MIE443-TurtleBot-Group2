0a23b51b6932372cd832f595afed6648d14e9cb6
Ediii-1 <edisonguo1924@gmail.com>
2025-02-06 13:05:46 -0500

    code for actual maze test

--- src/mie443_contest_1/mie443_contest_1/src/contest1.cpp
+++ src/mie443_contest_1/mie443_contest_1/src/contest1.cpp
@@ -4,7 +4,7 @@
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
 
-//Odometry header files
+// Odometry header files
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_datatypes.h>
 
@@ -16,20 +16,20 @@
 float angular = 0.0;
 float linear = 0.2;
 
-double posX=0.0, posY=0.0, yaw=0.0;
+double posX = 0.0, posY = 0.0, yaw = 0.0;
 
 #define N_BUMPER (3)
-#define RAD2DEG(rad)((rad)*180./M_PI)
-#define DEG2RAD(deg)((deg)*M_PI/180.)
+#define RAD2DEG(rad) ((rad) * 180. / M_PI)
+#define DEG2RAD(deg) ((deg) * M_PI / 180.)
 
-float minLaserDist = std::numeric_limits <float> ::infinity();
+float minLaserDist = std::numeric_limits<float>::infinity();
 int32_t nLasers = 0, desiredNLasers = 0, desiredAngle = 5;
 
-uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED,kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
 
-void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr &msg)
 {
-	//Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
+    // Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
     bumper[msg->bumper] = msg->state;
 }
 
@@ -39,92 +39,96 @@ void bumperMovement(geometry_msgs::Twist vel, ros::Publisher vel_pub)
     bool centerState = (bumper[kobuki_msgs::BumperEvent::CENTER] == kobuki_msgs::BumperEvent::PRESSED);
     bool rightState = (bumper[kobuki_msgs::BumperEvent::RIGHT] == kobuki_msgs::BumperEvent::PRESSED);
 
-    if (leftState) {
+    if (leftState)
+    {
         ROS_WARN("Left bumper hit! Moving backward...");
         vel.angular.z = 0.0;
         vel.linear.x = -0.1;
         vel_pub.publish(vel);
-        ros::Duration(1.5).sleep(); //robot move backward for 1 second
+        ros::Duration(1.5).sleep(); // robot move backward for 1 second
 
         ROS_WARN("Turning Right to find open space...");
-        vel.angular.z = -M_PI/3;
+        vel.angular.z = -M_PI / 3;
         vel.linear.x = 0.0;
         vel_pub.publish(vel);
-        ros::Duration(1.5).sleep(); 
+        ros::Duration(1.5).sleep();
     }
 
-    else if (centerState) {
+    else if (centerState)
+    {
         ROS_WARN("Center bumper hit! Moving backward...");
         vel.angular.z = 0.0;
         vel.linear.x = -0.1;
         vel_pub.publish(vel);
-        ros::Duration(1.5).sleep(); //robot move backward for 1 second
+        ros::Duration(1.5).sleep(); // robot move backward for 1 second
 
         ROS_WARN("Turning Left to find open space...");
-        angular = M_PI/2;
+        angular = M_PI / 2;
         linear = 0.0;
         vel.angular.z = angular;
         vel.linear.x = linear;
         vel_pub.publish(vel);
-        ros::Duration(1.5).sleep(); 
+        ros::Duration(1.5).sleep();
     }
 
-    else if (rightState) {
+    else if (rightState)
+    {
         ROS_WARN("Right bumper hit! Moving backward...");
         vel.angular.z = 0.0;
-        vel.linear.x = -0.1; //move backwward
+        vel.linear.x = -0.1; // move backwward
         vel_pub.publish(vel);
-        ros::Duration(1.5).sleep(); //robot move backward for 1 second
+        ros::Duration(1.5).sleep(); // robot move backward for 1 second
 
         ROS_WARN("Turning Left to find open space...");
-        vel.angular.z = M_PI/3;  //turn 60 degrees right
+        vel.angular.z = M_PI / 3; // turn 60 degrees right
         vel.linear.x = 0.0;
         vel_pub.publish(vel);
-        ros::Duration(1.5).sleep(); 
+        ros::Duration(1.5).sleep();
     }
 }
 
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
-	minLaserDist=std::numeric_limits<float>::infinity();
-    
-    nLasers=(msg->angle_max-msg->angle_min)/msg->angle_increment; // total number of lasers
-    desiredNLasers=DEG2RAD(desiredAngle)/msg->angle_increment;// index of the desired laser
-    //ROS_INFO("Size of laser scan array: %i and size of offset: %i",nLasers,desiredNLasers); // offset is the number of sectors from the center sector
+    ROS_INFO("laserCallback");
+    minLaserDist = std::numeric_limits<float>::infinity();
 
-    if(DEG2RAD(desiredAngle)<msg->angle_max && DEG2RAD(-desiredAngle)>msg->angle_min) //if the desired angle is within the size of the cone
+    nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment; // total number of lasers
+    desiredNLasers = DEG2RAD(desiredAngle) / msg->angle_increment;      // index of the desired laser
+    // ROS_INFO("Size of laser scan array: %i and size of offset: %i",nLasers,desiredNLasers); // offset is the number of sectors from the center sector
+
+    if (DEG2RAD(desiredAngle) < msg->angle_max && DEG2RAD(-desiredAngle) > msg->angle_min) // if the desired angle is within the size of the cone
     {
-        for(uint32_t laser_idx = nLasers/2-desiredNLasers; laser_idx < nLasers/2 + desiredNLasers;++laser_idx)
+        for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx)
         {
-            //ROS_INFO("Min laser dist: %f, msg->ranges[laser_idx]: %f", minLaserDist, msg->ranges[laser_idx]);
+            // ROS_INFO("Min laser dist: %f, msg->ranges[laser_idx]: %f", minLaserDist, msg->ranges[laser_idx]);
 
-            minLaserDist=std::min(minLaserDist, msg->ranges[laser_idx]);
+            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
         }
 
-        if(minLaserDist==std::numeric_limits <float> ::infinity())
+        if (minLaserDist == std::numeric_limits<float>::infinity())
         {
-            minLaserDist=0;
+            minLaserDist = 0;
         }
-        ROS_INFO("Minimum Laser Dist: %f",minLaserDist);
+        ROS_INFO("Minimum Laser Dist: %f", minLaserDist);
     }
     else // if the desired angle is not in the cone (probably not useful)
     {
-        for(uint32_t laser_idx = 0; laser_idx<nLasers; ++laser_idx)
+        for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx)
         {
             minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
         }
         // ROS_INFO("Minimum Laser Dist: %i",minLaserDist);
-        
-        if(minLaserDist==std::numeric_limits <float> ::infinity())
+
+        if (minLaserDist == std::numeric_limits<float>::infinity())
         {
-            minLaserDist=0;
+            minLaserDist = 0;
         }
     }
-    
-    ros::Duration(0.5).sleep();
+
+    // ros::Duration(0.5).sleep();
 }
 
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
 {
     posX = msg->pose.pose.position.x;
     posY = msg->pose.pose.position.y;
@@ -133,20 +137,23 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 }
 
 // Random Exploration - Moves and Turns Randomly
-void explore(geometry_msgs::Twist &vel, ros::Publisher &vel_pub) {
-    if (minLaserDist < 0.8) {  // If an obstacle is close
+void explore(geometry_msgs::Twist &vel, ros::Publisher &vel_pub)
+{
+    if (minLaserDist < 0.8)
+    { // If an obstacle is close
         ROS_WARN("Obstacle detected! Turning...");
         vel.linear.x = 0.0;
-        vel.angular.z = (rand() % 2 == 0) ? M_PI / 6 : -M_PI / 6;  // Random turn direction
+        vel.angular.z = (rand() % 2 == 0) ? M_PI / 2 : -M_PI / 2; // Random turn direction
         vel_pub.publish(vel);
-        ros::Duration(1.5).sleep();
-    } else {
-        vel.linear.x = 0.2;  // Move forward
-        vel.angular.z = ((rand() % 10) == 0) ? ((rand() % 2 == 0) ? M_PI / 6 : -M_PI / 6) : 0.0;  // Occasional random turn
+        ros::Duration(0.5).sleep();
+    }
+    else
+    {
+        vel.linear.x = 0.2;                                                                      // Move forward
+        vel.angular.z = ((rand() % 10) == 0) ? ((rand() % 2 == 0) ? M_PI / 6 : -M_PI / 6) : 0.0; // Occasional random turn
         vel_pub.publish(vel);
         ros::Duration(1.5).sleep();
     }
-
 }
 
 int main(int argc, char **argv)
@@ -173,9 +180,10 @@ int main(int argc, char **argv)
     float angular = 0.0;
     float linear = 0.2;
 
-    while(ros::ok() && secondsElapsed <= 480) {
+    while (ros::ok() && secondsElapsed <= 480)
+    {
         ros::spinOnce();
-        
+
         // //Check if any of the bumpers were pressed
         // bool any_bumper_pressed = false;
         // for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx){
@@ -190,7 +198,7 @@ int main(int argc, char **argv)
         vel_pub.publish(vel);
 
         // The last thing to do is to update the timer.
-        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start).count();
         loop_rate.sleep();
     }
 
