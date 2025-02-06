5dd992b83466ebe44991d7993f693cb31a42d183
ediii-1 <edisonguo1924@gmail.com>
2025-02-06 13:43:51 -0500

    update lasercallback

--- src/mie443_contest_1/mie443_contest_1/src/contest1.cpp
+++ src/mie443_contest_1/mie443_contest_1/src/contest1.cpp
@@ -22,8 +22,6 @@ double posX = 0.0, posY = 0.0, yaw = 0.0;
 #define RAD2DEG(rad) ((rad) * 180. / M_PI)
 #define DEG2RAD(deg) ((deg) * M_PI / 180.)
 
-float minLaserDist = std::numeric_limits<float>::infinity();
-int32_t nLasers = 0, desiredNLasers = 0, desiredAngle = 5;
 
 uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
 
@@ -87,45 +85,110 @@ void bumperMovement(geometry_msgs::Twist vel, ros::Publisher vel_pub)
     }
 }
 
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
+float minLaserDistCenter = std::numeric_limits <float> ::infinity();
+float minLaserDistLeft = std::numeric_limits <float> ::infinity();
+float minLaserDistRight = std::numeric_limits <float> ::infinity();
+int32_t nLasers = 0, desiredNLasersCenter = 0, desiredNLasersLeft = 0, desiredNLasersRight = 0, desiredAngleCenter = 10,desiredAngleRight = 10,desiredAngleLeft = 10;
+
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    ROS_INFO("laserCallback");
-    minLaserDist = std::numeric_limits<float>::infinity();
+	minLaserDistCenter=std::numeric_limits<float>::infinity();
+    minLaserDistLeft=std::numeric_limits<float>::infinity();
+    minLaserDistRight=std::numeric_limits<float>::infinity();
 
-    nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment; // total number of lasers
-    desiredNLasers = DEG2RAD(desiredAngle) / msg->angle_increment;      // index of the desired laser
-    // ROS_INFO("Size of laser scan array: %i and size of offset: %i",nLasers,desiredNLasers); // offset is the number of sectors from the center sector
+    nLasers=(msg->angle_max-msg->angle_min)/msg->angle_increment; // total number of laser indices
+    desiredNLasersCenter=DEG2RAD(desiredAngleCenter)/msg->angle_increment;// number of indices to search in center
+    desiredNLasersLeft=DEG2RAD(desiredAngleLeft)/msg->angle_increment;// number of indices to search on the left
+    desiredNLasersRight=DEG2RAD(desiredAngleRight)/msg->angle_increment;// number of indices to search on the right
+    //ROS_INFO("Size of laser scan array: %i and size of offset: %i",nLasers,desiredNLasersCenter); // offset is the number of sectors from the center sector
 
-    if (DEG2RAD(desiredAngle) < msg->angle_max && DEG2RAD(-desiredAngle) > msg->angle_min) // if the desired angle is within the size of the cone
+//Minimum center distance
+    if(DEG2RAD(desiredAngleCenter)<msg->angle_max && DEG2RAD(-desiredAngleCenter)>msg->angle_min) //if the desired angle is within the size of the cone
     {
-        for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx)
+        for(uint32_t laser_idx = nLasers/2-desiredNLasersCenter; laser_idx < nLasers/2 + desiredNLasersCenter;++laser_idx) // Center sector
         {
-            // ROS_INFO("Min laser dist: %f, msg->ranges[laser_idx]: %f", minLaserDist, msg->ranges[laser_idx]);
+            //ROS_INFO("Min laser dist: %f, msg->ranges[laser_idx]: %f", minLaserDistCenter, msg->ranges[laser_idx]);
 
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+            minLaserDistCenter=std::min(minLaserDistCenter, msg->ranges[laser_idx]);
         }
 
-        if (minLaserDist == std::numeric_limits<float>::infinity())
+        if(minLaserDistCenter==std::numeric_limits <float> ::infinity())
         {
-            minLaserDist = 0;
+            minLaserDistCenter=0;
         }
-        ROS_INFO("Minimum Laser Dist: %f", minLaserDist);
+        ROS_INFO("Minimum Laser Dist CENTER: %f",minLaserDistCenter);
     }
+    
     else // if the desired angle is not in the cone (probably not useful)
     {
-        for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx)
+        for(uint32_t laser_idx = 0; laser_idx<nLasers; ++laser_idx)
         {
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+            minLaserDistCenter = std::min(minLaserDistCenter, msg->ranges[laser_idx]);
         }
-        // ROS_INFO("Minimum Laser Dist: %i",minLaserDist);
-
-        if (minLaserDist == std::numeric_limits<float>::infinity())
+        // ROS_INFO("Minimum Laser Dist: %i",minLaserDistCenter);
+        
+        if(minLaserDistCenter==std::numeric_limits <float> ::infinity())
         {
-            minLaserDist = 0;
+            minLaserDistCenter=0;
         }
     }
 
-    // ros::Duration(0.5).sleep();
+//Minimum distance on the right
+    if(DEG2RAD(desiredAngleRight)<((msg->angle_max)-(msg->angle_min)))
+    {
+        for(uint32_t laser_idx=0; laser_idx<desiredNLasersRight; ++laser_idx)
+        {
+            minLaserDistRight=std::min(minLaserDistRight, msg->ranges[laser_idx]);
+        }
+        if(minLaserDistRight==std::numeric_limits <float> ::infinity())
+        {
+            minLaserDistRight=0;
+        }
+        ROS_INFO("Minimum Laser Dist RIGHT: %f",minLaserDistRight);
+    }
+    else{
+        for(uint32_t laser_idx = 0; laser_idx<nLasers; ++laser_idx)
+        {
+            minLaserDistRight = std::min(minLaserDistRight, msg->ranges[laser_idx]);
+        }       
+        if(minLaserDistRight==std::numeric_limits <float> ::infinity())
+        {
+            minLaserDistRight=0;
+        }
+        ROS_INFO("Minimum Laser Dist RIGHT: %f",minLaserDistRight);
+    }
+
+//Minimum distance on the left
+    if(DEG2RAD(desiredAngleLeft)<((msg->angle_max)-(msg->angle_min)))
+    {
+        for(uint32_t laser_idx=nLasers-1; laser_idx>(nLasers-1)-desiredNLasersLeft; --laser_idx)
+        {
+            minLaserDistLeft=std::min(minLaserDistLeft, msg->ranges[laser_idx]);
+        }
+        if(minLaserDistLeft==std::numeric_limits <float> ::infinity())
+        {
+                minLaserDistLeft=0;
+        }
+
+        ROS_INFO("Minimum Laser Dist LEFT: %f",minLaserDistLeft);
+    }
+    else
+    {
+        for(uint32_t laser_idx = 0; laser_idx<nLasers; ++laser_idx)
+        {
+            minLaserDistLeft = std::min(minLaserDistLeft, msg->ranges[laser_idx]);
+        }
+
+        if(minLaserDistLeft==std::numeric_limits <float> ::infinity())
+        {
+            minLaserDistLeft=0;
+        }
+
+        ROS_INFO("Minimum Laser Dist LEFT: %f",minLaserDistLeft);
+    }
+
+    
+    ros::Duration(0.5).sleep();
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
