cf5a935c805629f25a1edbbc0c2241bde4768f3c
ediii-1 <edisonguo1924@gmail.com>
2025-03-06 11:44:20 -0500

    added missing codes

--- src/mie443_contest2/src/imagePipeline.cpp
+++ src/mie443_contest2/src/imagePipeline.cpp
@@ -134,6 +134,32 @@
 #include <opencv2/features2d.hpp>
 #include <opencv2/flann.hpp>
 
+#include <imagePipeline.h>
+
+#define IMAGE_TYPE sensor_msgs::image_encodings::BGR8
+#define IMAGE_TOPIC "camera/rgb/image_raw" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
+
+ImagePipeline::ImagePipeline(ros::NodeHandle& n) {
+    image_transport::ImageTransport it(n);
+    sub = it.subscribe(IMAGE_TOPIC, 1, &ImagePipeline::imageCallback, this);
+    isValid = false;
+}
+
+void ImagePipeline::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+    try {
+        if(isValid) {
+            img.release();
+        }
+        img = (cv_bridge::toCvShare(msg, IMAGE_TYPE)->image).clone();
+        isValid = true;
+    } catch (cv_bridge::Exception& e) {
+        std::cout << "ERROR: Could not convert from " << msg->encoding.c_str()
+                  << " to " << IMAGE_TYPE.c_str() << "!" << std::endl;
+        isValid = false;
+    }    
+}
+
+
 int ImagePipeline::getTemplateID(Boxes& boxes) {
     int template_id = -1;
     if(!isValid) {
@@ -202,5 +228,3 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
     }  
     return template_id;
 }
-
-#endif
