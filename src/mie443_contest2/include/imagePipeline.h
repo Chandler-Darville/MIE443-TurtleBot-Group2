d2b7a857d181e8faae5e5dcd947b8fbab959a294
YC-Wang21 <leo_wangyicheng@outlook.com>
2025-03-13 12:39:46 -0400

    Changed header file to default

--- src/mie443_contest2/include/imagePipeline.h
+++ src/mie443_contest2/include/imagePipeline.h
@@ -16,9 +16,4 @@ class ImagePipeline {
         ImagePipeline(ros::NodeHandle& n);
         void imageCallback(const sensor_msgs::ImageConstPtr& msg);
         int getTemplateID(Boxes& boxes);
-
-        //-------------added-------------------
-        bool isImageValid() const;
-        void resetImageValidity(); 
-        //-------------------------------------
 };
